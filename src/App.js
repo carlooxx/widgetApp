938a9dfedea602ca6418d54a71dddca76cd0c4e5
carlooxx <karlo.braovac@gmail.com>
2021-01-14 23:33:58 +0100

    Dropdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React, { useState } from "react";
+// import Accordion from "./components/Accordion";
+// import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items = [
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use Reaact?",
+//     content: "You use Reacy by creating components",
+//   },
+// ];
+const options = [
   {
-    title: "What is React?",
-    content: "React is front end javascript framework",
+    label: "This is color red",
   },
   {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
+    label: "This is color blue",
   },
   {
-    title: "How do you use Reaact?",
-    content: "You use Reacy by creating components",
+    label: "This is color black",
   },
 ];
-
 function App() {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="App">
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 }
