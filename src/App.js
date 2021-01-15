27c679d5eed2841d681abaccfc1f58327f157668
carlooxx <karlo.braovac@gmail.com>
2021-01-15 15:53:34 +0100

    Navigation and Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-// import Accordion from "./components/Accordion";
-// import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Header from "./components/Header";
+import { Route, Switch } from "react-router-dom";
 
-// const items = [
-//   {
-//     title: "What is React?",
-//     content: "React is front end javascript framework",
-//   },
-//   {
-//     title: "Why use React?",
-//     content: "React is a favorite JS library among engineers",
-//   },
-//   {
-//     title: "How do you use Reaact?",
-//     content: "You use Reacy by creating components",
-//   },
-// ];
+const items = [
+  {
+    title: "What is React?",
+    content: "React is front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use Reaact?",
+    content: "You use Reacy by creating components",
+  },
+];
 const options = [
   {
     label: "This is color red",
@@ -29,10 +31,31 @@ const options = [
     label: "This is color black",
   },
 ];
+
 function App() {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="App">
-      <Translate />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Accordion items={items} />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown
+            label="Select Color"
+            options={options}
+            selected={selected}
+            onSelectedChange={setSelected}
+          />
+        </Route>
+        <Route path="/translate">
+          <Translate />
+        </Route>
+      </Switch>
     </div>
   );
 }
